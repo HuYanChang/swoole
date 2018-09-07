b3a5bd08a859da06790f609038d5f14256374cf3
hyc <976262957@qq.com>
2018-09-07 17:11:54 +0800

    hyc

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,7 @@
  * Time: 16:52
  */
 //$app = new swoole_http_server('127.0.0.1',8081);
-$app = new swoole_server(string "127.0.0.1", int $port = 9051, int $mode = SWOOLE_PROCESS,
+$app = new swoole_server(string "*.*.*.*", int $port = 9051, int $mode = SWOOLE_PROCESS,
     int $sock_type = SWOOLE_SOCK_TCP);
 
 
